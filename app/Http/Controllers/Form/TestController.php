2edc9777eb45676fc25d0ebc7866983d78c64c22
francisco1030 <franciscoviana862@gmail.com>
2020-05-10 11:34:46 -0300

    format date;

--- app/Http/Controllers/Form/TestController.php
+++ app/Http/Controllers/Form/TestController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Form;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class TestController extends Controller
 {
@@ -19,9 +21,21 @@ class TestController extends Controller
 
     }
 
+    public function create()
+    {
+        $user = new User();
+        $user->name = "Costa Lima";
+        $user->email = "costal@gmail.com";
+        $user->password = Hash::make("123");
+        $user->save();
+    }
+
     public function getUser(User $user) 
     {
-        var_dump($user);
+       // var_dump($user);
+       return view('list-user', [
+            'user' => $user
+       ]);
 
     }
 }
--- resources/views/list-user.blade.php
+++ resources/views/list-user.blade.php
@@ -1,4 +1,20 @@
-<h1> Listagem de Usuário</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
 
-<p>Nome: {{ $user->name }} </p>
+  <body>
+
+    <h3> Listagem de Usuário</h3>
+
+    <p>Nome: {{ $user->name }} </p>
+    <p>E-mail: {{ $user->email }} </p>
+    <p>Data: {{ date('d/m/Y H:i', strtotime($user->created_at)) }} </p>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
 
